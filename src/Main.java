fc46eecfd74ee1b012b925eaebe174fffb8d88d7
1234 <12345>
2020-10-24 14:47:24 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt(), B = in.nextInt();
+        System.out.println(A + B);
+    }
 }
